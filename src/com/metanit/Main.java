3733cc20a08a233d2a52811e859155719f5dabd5
Dmitriy <dima.ermolovich04@gmail.com>
2021-11-02 19:14:24 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,12 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array=new int[10];
+        array[3]=7;
+        System.out.println(array[3]);
+
+    }
+}
